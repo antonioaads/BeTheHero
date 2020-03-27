dc1cc39744965d5fcc09aae35dff9d52d752e494
Antônio Sousa <antonioaads@gmail.com>
2020-03-27 10:00:15 -0300

    Organização das rotas em controllers, criação de rota index e create para ongs e casos

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -17,27 +17,17 @@
 */
 
 const express = require('express');
-const crypto = require('crypto');
 
-const connection = require('./database/connection');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
-routes.post('/ongs', (request, response) => {
-    const { name, email, whatsapp, city, uf } = request.body;
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-    const id = crypto.randomBytes(4).toString('HEX');
-    
-    connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf
-    });
-
-    return response.json();
-});
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
