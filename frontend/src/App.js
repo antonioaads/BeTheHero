c047b1b51e786982986b462e9568b5a6f3f642d0
Antônio Sousa <antonioaads@gmail.com>
2020-03-28 11:00:16 -0300

    Adicionado estilo global, criação do layout da página de logon e de cadastro

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import './global.scss'
+
+import Routes from './routes';
+
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <Routes/>
   );
 }
 
